17cf0de4b52a177787d74273b70d40d23f55ee65
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-01-23 14:44:04 +0000

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,23 @@
+package com.ait.ex6B2;
+
+public class Person {
+	private int age;
+	private String name;
+	
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	@Override
+	public String toString() {
+		return "Person [age="+age+", name="+name+"]";
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
